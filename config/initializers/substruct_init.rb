8d1181287d41234617ba801994dfb8aaa173bb28
Laurence A. Lee <rubyjedi@gmail.com>
2010-07-13 12:17:44 -1000

    Move Substruct initialization from config/environment.rb in Main App to an initializer inside Substruct Plugin.

---
+++ config/initializers/substruct_init.rb
@@ -0,0 +1,13 @@
+# Substruct Globals
+ERROR_EMPTY  = 'Please fill in this field.'
+ERROR_NUMBER = 'Please enter only numbers (0-9) in this field.'
+
+## Substruct Init (TODO: Make this into a config/initializer) ##
+begin
+  Preference.init_mail_settings()
+rescue
+  puts "[SUBSTRUCT-MAILER WARNING 2010]"
+  puts "Mail server settings have not been initialized."
+  puts "Check to make sure they've been set in the admin panel."
+  # Don't care if this bombs out, because initially this won't have a value.
+end

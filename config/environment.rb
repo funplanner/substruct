235d07ced32328c015fb250173baaf9be3f9382e
subimage <subimage@6a1f77cc-a145-0410-afd8-bd7963fcc59f>
2010-08-27 23:27:28 +0000

    Moved globals and preference loading to plugin initialization and out of environment.rb Stupid to clutter up that file if we don't have to.
    git-svn-id: http://substruct.googlecode.com/svn/trunk/plugins/substruct@304 6a1f77cc-a145-0410-afd8-bd7963fcc59f

--- config/environment.rb
+++ config/environment.rb
@@ -25,16 +25,3 @@ Rails::Initializer.run do |config|
   config.gem 'mini_magick', :version => '1.3.3'
   config.gem 'ezcrypto'
 end
-
-begin
-  Preference.init_mail_settings()
-rescue
-  puts "[SUBSTRUCT WARNING]"
-  puts "Mail server settings have not been initialized."
-  puts "Check to make sure they've been set in the admin panel."
-  # Don't care if this bombs out, because initially this won't have a value.
-end
-
-# Globals
-ERROR_EMPTY  = 'Please fill in this field.'
-ERROR_NUMBER = 'Please enter only numbers (0-9) in this field.'
--- lib/substruct.rb
+++ lib/substruct.rb
@@ -1,8 +1,20 @@
 # Copyright (c) 2010 Subimage LLC
 # http://www.subimage.com
 require_dependency 'substruct/login_system.rb'
-
 require 'patch_attachment_fu'
+
+# Globals
+ERROR_EMPTY  = 'Please fill in this field.'
+ERROR_NUMBER = 'Please enter only numbers (0-9) in this field.'
+
+begin
+  Preference.init_mail_settings()
+rescue
+  puts "[SUBSTRUCT WARNING]"
+  puts "Mail server settings have not been initialized."
+  puts "Check to make sure they've been set in the admin panel."
+  # Don't care if this bombs out, because initially this won't have a value.
+end
 
 module Substruct
   # Should we use live rate calculation via FedEx?

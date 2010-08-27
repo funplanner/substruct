40ce68a72726022b4ee4825a0ef77e4459413d35
subimage <subimage@6a1f77cc-a145-0410-afd8-bd7963fcc59f>
2010-08-27 03:47:15 +0000

    Bumping version official for 1.3
    git-svn-id: http://substruct.googlecode.com/svn/trunk/plugins/substruct@293 6a1f77cc-a145-0410-afd8-bd7963fcc59f

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
 # Copyright (c) 2010 Subimage LLC
 # http://www.subimage.com
 require 'substruct'
+require 'substruct_deprecated'
--- lib/substruct/version.rb
+++ lib/substruct/version.rb
@@ -2,7 +2,7 @@ module Substruct
   module Version
     MAJOR  = 1
     MINOR  = 3
-    TINY   = 'a'
+    TINY   = 0
     STRING = [MAJOR, MINOR, TINY].join('.').freeze
   end
 end

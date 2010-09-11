b1870eb8de4c77a103230cf243267128cf74fb2a
subimage <subimage@6a1f77cc-a145-0410-afd8-bd7963fcc59f>
2010-09-11 08:04:07 +0000

    Added simple (as can be) HTML email for sending receipts to customers.
    It makes sense now, since we allow TinyMCE to insert HTML into the "thank you for your order" snippet.
    
    Order receipt is sent as a multi-part email, with text as before, and HTML email. The HTML snippet is stripped of HTML tags to be rendered in plain text.
    
    Still need to come up with a good way to inline CSS styles for older mail clients, but leaving that for another day.
    
    Closes issue 215.
    
    
    
    git-svn-id: http://substruct.googlecode.com/svn/trunk/plugins/substruct@346 6a1f77cc-a145-0410-afd8-bd7963fcc59f

--- app/models/orders_mailer.rb
+++ app/models/orders_mailer.rb
@@ -1,51 +1,69 @@
 class OrdersMailer < ActionMailer::Base
   helper :application
-
+  
   def inquiry(addy_from, email_text)
-    setup_defaults()
-    @subject  = "Inquiry from the site"
-    @body     = {:from => addy_from, :email_text => email_text}
-    @recipients = Preference.get_value('mail_copy_to').split(',')
-    @from = addy_from
+    setup_defaults
+    
+    subject      "Inquiry from the site"
+    body         :from => addy_from, :email_text => email_text
+    recipients   Preference.get_value('mail_copy_to').split(',')
+    from         addy_from
   end
 
   def receipt(order, email_text)
-    @subject = "Thank you for your order! (\##{order.order_number})"
-    @body       = {:order => order, :email_text => email_text}
-    @recipients = order.order_user.email_address
     setup_defaults
-  end
 
-  def reset_password(customer)
-    @subject = "Password reset for #{Preference.get_value('store_name')}"
-    @body       = {:customer => customer}
-    @recipients = customer.email_address
-    setup_defaults
+    recipients   order.order_user.email_address    
+    subject      "Thank you for your order! (\##{order.order_number})"
+    content_type "multipart/alternative"
+
+    @body = { :order => order, :email_text => email_text }
+
+    part "text/plain" do |p|
+      p.body = render_message(
+        "receipt.text.plain", 
+        @body
+      )
+      p.transfer_encoding = "base64"
+    end
+    part "text/html" do |p|
+      p.body = render_message(
+        "receipt.text.html", 
+        @body
+      )
+    end
   end
 
   def failed(order)
-    @subject = "An order has failed on the site"
-    @body       = {:order => order}
-    @recipients = Preference.get_value('mail_copy_to').split(',')
-    setup_defaults    
-  end
-  
-  def testing array_to
-    @subject = "Test from #{Preference.get_value('store_name')}"
-    #@body       = {:order => order}
-    @recipients = array_to
     setup_defaults
-    # we get here
+    
+    recipients   Preference.get_value('mail_copy_to').split(',')
+    subject      "An order has failed on the site"
+    body         :order => order
+
+  end
+
+  def reset_password(customer)
+    setup_defaults
+    
+    recipients   customer.email_address
+    subject      "Password reset for #{Preference.get_value('store_name')}"
+    body         :customer => customer
+  end
+
+  def testing(array_email_addresses)
+    setup_defaults
+    
+    subject      "Test from #{Preference.get_value('store_name')}"
+    recipients   array_email_addresses
   end
   
   private
-  def setup_defaults
-  
-    Preference.get_value('mail_username')
-    @bcc        = Preference.get_value('mail_copy_to').split(',')
-    @from       = Preference.get_value('mail_username')
-    @sent_on    = Time.now
-    @headers    = {}
-  end
+    def setup_defaults
+      @bcc        = Preference.get_value('mail_copy_to').split(',')
+      @from       = Preference.get_value('mail_username')
+      @sent_on    = Time.now
+      @headers    = {}
+    end
   
 end
